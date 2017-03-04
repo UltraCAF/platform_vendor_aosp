6e8eebfeaf030cc451b7900c438b03791934db0a
offain <fabian.leutenegger@bluewin.ch>
2017-03-04 23:32:58 +0100

    vendor/aosp: Add missing RCS packages
    These are present in caf based device trees, but not on others.
    
    Change-Id: I031f2adfcabf121e7734e17a920c1e2fc489d645

--- common.mk
+++ common.mk
@@ -93,6 +93,9 @@ PRODUCT_BOOT_JARS += \
 
 # RCS
 PRODUCT_PACKAGES += \
+    rcscommon \
+    rcscommon.xml \
+    rcsservice \
     rcs_service_aidl \
     rcs_service_aidl.xml \
     rcs_service_aidl_static \
